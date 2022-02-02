05b1aae5c711071557a7d226ab4af7cca5523fc7
Mateusz Oleś <moles9@wp.pl>
2022-02-02 21:21:55 +0100

    Prepared code to writing text in text file

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("Hello World");
+    FILE *fprt;
+
+    // use apropriate locations
+    fprt = fopen("D:/Praca/Projekty/Repozytoria/Tekst.txt","w");
+    if(fprt==NULL)
+    {
+        printf("Error");
+        exit(1);
+    }
+    fprintf(fprt,"Hello World");
+    fclose(fprt);
 }
