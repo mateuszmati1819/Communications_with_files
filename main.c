bd6d5f7d78e8f224e6acad1d60d247fca19e3761
Mateusz Oleś <moles9@wp.pl>
2022-02-06 22:03:51 +0100

    Created program  which can read text. Modified .gitignore file

--- main.c
+++ main.c
@@ -4,14 +4,23 @@
 int main()
 {
     FILE *fprt;
+    int data;
+    char singleline[150];
+    
 
     // use apropriate locations
-    fprt = fopen("D:/Praca/Projekty/Repozytoria/Tekst.txt","w");
+    fprt = fopen("D:/Praca/Projekty/Repozytoria/Tekst.txt","r");
     if(fprt==NULL)
     {
         printf("Error");
         exit(1);
     }
-    fprintf(fprt,"Hello World");
+    while(!feof(fprt))
+    {
+        fgets(singleline, 150, fprt);
+        puts(singleline);
+    }
+    
+
     fclose(fprt);
 }
