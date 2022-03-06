92f6a1062f4c31a2d2dd936db5c6807a31524ef3
Mateusz Oleś <moles9@wp.pl>
2022-03-06 19:24:58 +0100

    Creating shapes like square and letters. In this release you can find commented code to generating square and code to generating M letter

--- main.c
+++ main.c
@@ -1,53 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include "functions.h"
-
-
+ 
+ 
 int main()
 {
-    int letters;
-    clearScreen();
-
-    FILE *fprt;
-    int data;
-    char singleline[150];
-    int tablica[5];
-
-    fprt = fopen("D:/Praca/Projekty/Repozytoria/Tekst.txt","r");
-    if(fprt==NULL)
-    {
-        printf("Error");
-        exit(1);
-    }
-    while(!feof(fprt))
-    {
-        fgets(singleline, 150, fprt);
-        letters = count_letters(singleline);
-    }
-
-    printf("%s, amount of letters is %d",singleline, letters);
+     printf("\e[1;1H\e[2J");
+    // int arr[10]={1,1,2,3,5,8,13,21,34,55};
+    // int *ptr=&arr;
+    // printf("%i",*ptr);
     
-
-    fclose(fprt);
-
-    printf(" \nTest for venture functions: \n");
-    // printf("Drawn number is: %d", venture_number(5));
-    
-    srand(time(NULL));
     for(int i =0; i<5;i++)
     {
-        tablica[i]=venture_number(49);
+        if(i==0||i==3||i==4)
+        {
+            for(int j=0;j<5;j++)
+            {
+                if(j==0||j==4)
+                {
+                    printf("*");
+                }
+                
+                else
+                {
+                    printf(" ");
+                }
+                
+            }
+        }
+        if(i==1)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==2)
+                    {
+                        printf(" ");
+                    }
+                    else
+                    {
+                        printf("*");
+                    }
+                    
+                }   
+            }
+            if(i==2)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    if(j==0||j==2||j==4)
+                    {
+                        printf("*");
+                    }
+                    else
+                    {
+                        printf(" ");
+                    } 
+                }
+            }
+            printf("\n");
     }
-    for(int i =0; i<5;i++)
-    {
-        printf("%d \n", tablica[i]);
-    }
-    
-    tablica[0]=lotto_numbers(tablica[5]);
-    printf("Check of function lotto_numbers: %d",tablica[0]);
+    printf("\n");
+    printf("*   *\n");
+    printf("** **\n");
+    printf("* * *\n");
+    printf("*   *\n");
 
-    return 0;
+    // int size =10;
+
+    // for(int i=0;i<(size+1);i++)
+    // {
+    //     if(i==0 || i==size)
+    //     {
+    //         for(int j=0;j<(size+1);j++)
+    //         {
+    //             printf("* ");
+    //         }
+    //     }
+    //     else
+    //     {
+    //         for(int k=0;k<(size+1);k++)
+    //         {
+    //             if(k==0 || k==size)
+    //             {
+    //                 printf("* ");
+    //             }
+    //             else
+    //             {
+    //                 printf("  ");
+    //             }
+    //         }
+    //     }
+    //     printf("\n");
+    // }
+
+
 }
-
---
+++ system_command.bat
@@ -0,0 +1,3 @@
+echo "Hello Mateusz"
+#start www.youtube.pl
+start C:\Program Files\Blender Foundation\Blender 2.92
